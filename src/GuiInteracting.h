0f756f53da393e44bc83c430161fdb1b55829d70
Joe Natter <office@dsp-crowd.com>
2025-08-18 22:48:12 +0200

    src: Add files

---
+++ src/GuiInteracting.h
@@ -0,0 +1,72 @@
+/*
+  This file is part of the DSP-Crowd project
+  https://www.dsp-crowd.com
+
+  Author(s):
+      - Johannes Natter, office@dsp-crowd.com
+
+  File created on 18.08.2025
+
+  Copyright (C) 2025, Johannes Natter
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef GUI_INTERACTING_H
+#define GUI_INTERACTING_H
+
+#include "Processing.h"
+
+class GuiInteracting : public Processing
+{
+
+public:
+
+	static GuiInteracting *create()
+	{
+		return new dNoThrow GuiInteracting;
+	}
+
+protected:
+
+	virtual ~GuiInteracting() {}
+
+private:
+
+	GuiInteracting();
+	GuiInteracting(const GuiInteracting &) = delete;
+	GuiInteracting &operator=(const GuiInteracting &) = delete;
+
+	/*
+	 * Naming of functions:  objectVerb()
+	 * Example:              peerAdd()
+	 */
+
+	/* member functions */
+	Success process();
+	void processInfo(char *pBuf, char *pBufEnd);
+
+	/* member variables */
+	//uint32_t mStartMs;
+
+	/* static functions */
+
+	/* static variables */
+
+	/* constants */
+
+};
+
+#endif
+

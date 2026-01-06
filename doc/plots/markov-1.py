56619f53ea173b2d0d4629b4df1039a731acb271
Joe Natter <office@dsp-crowd.com>
2026-01-06 18:58:33 +0100

    doc: Update markov-1

--- doc/plots/markov-1.py
+++ doc/plots/markov-1.py
@@ -5,33 +5,43 @@ import argparse
 
 np.set_printoptions(formatter = {'float': '{:.3f}'.format})
 
-P = np.array([[0.14, 0.67],
+x = np.zeros((2, 1))
+
+A = np.array([[0.14, 0.67],
               [0.86, 0.33]])
 
 nDef = 1
 
 def matrix_power(n):
-	r = np.linalg.matrix_power(P, n)
 
-	print(f"P:")
-	print(P)
+	x[0] = 1.00
+	print(f"x:")
+	print(x)
 
-	print(f"R = P^{n}:")
-	print(r)
+	print(f"A:")
+	print(A)
 
-	sums = np.sum(r, axis = 0)
-	print(f"Sums:")
+	sums = np.sum(A, axis = 0)
+	print(f"Sums A:")
 	print(f" {sums}")
 
-	d = np.diag(r)
-	print(f"d = diag(R): ")
-	print(f" {d}")
+	R = np.linalg.matrix_power(A, n)
+	print(f"R = A^{n}:")
+	print(R)
 
-	return r
+	sums = np.sum(R, axis = 0)
+	print(f"Sums R:")
+	print(f" {sums}")
+
+	xn = R @ x
+	print(f"xn = R * x:")
+	print(xn)
+
+	return xn
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description = 'Calculating P^n')
-	parser.add_argument('n', type = int, nargs = '?', default = nDef, help = f"Exponent for P^n (default: {nDef})")
+	parser = argparse.ArgumentParser(description = 'Calculating A^n')
+	parser.add_argument('n', type = int, nargs = '?', default = nDef, help = f"Exponent for A^n (default: {nDef})")
 
 	args = parser.parse_args()
 	matrix_power(args.n)
